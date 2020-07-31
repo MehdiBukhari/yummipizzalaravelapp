11fbbe2b69490a2ef4d71d2adaacd6fc75b4ed47
Mehdi Bukhari <mehdi@maservices-ltd.com>
2020-07-31 21:14:32 +0500

    Required entites

---
+++ app/Http/Controllers/FooditemController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\fooditem;
+use Illuminate\Http\Request;
+
+class FooditemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\fooditem  $fooditem
+     * @return \Illuminate\Http\Response
+     */
+    public function show(fooditem $fooditem)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\fooditem  $fooditem
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(fooditem $fooditem)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\fooditem  $fooditem
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, fooditem $fooditem)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\fooditem  $fooditem
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(fooditem $fooditem)
+    {
+        //
+    }
+}
