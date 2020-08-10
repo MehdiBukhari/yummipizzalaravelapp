ee7db888c69d07e0a5dffb6341a30a2bedf3a9ff
Mehdi Bukhari <mehdi@maservices-ltd.com>
2020-08-10 16:52:41 +0500

    dsadfa

--- routes/api.php
+++ routes/api.php
@@ -21,8 +21,8 @@ use Illuminate\Support\Facades\Route;
 Route::group([
     'prefix' => 'auth'
 ], function () {
-    Route::post('/login', 'AuthController@login')->name('login');
-    Route::post('/signup', 'AuthController@signup');
+    Route::post('login', 'AuthController@login')->name('login');
+    Route::post('signup', 'AuthController@signup');
   
     Route::group([
       'middleware' => 'auth:api'
